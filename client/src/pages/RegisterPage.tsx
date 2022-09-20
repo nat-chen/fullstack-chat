1013b62e111d612d1b9f27346a1042e611eb0f98
Nat Chen <chen_natalie@outlook.com>
2022-09-20 16:42:38 +0800

    React: Create RegisterPage & RegisterForm Component

---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from "../components/forms/RegisterForm";
+import { Page } from '../utils/styles/index';
+
+export const AuthenticationPage = () => {
+  return (
+    <Page>
+      <RegisterForm />
+    </Page>
+  )
+}
