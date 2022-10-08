111b09c8408439e05e2e2accf789d63b429847f3
Nat Chen <chen_natalie@outlook.com>
2022-10-08 15:29:45 +0800

    Fixed message text from overflowing, form field width

--- client/src/utils/styles/index.tsx
+++ client/src/utils/styles/index.tsx
@@ -245,6 +245,7 @@ export const MessageItemContainer = styled.div`
   gap: 20px;
   align-items: center;
   padding: 5px 0;
+  word-break: break-all;
 `;
 
 export const MessageItemAvatar = styled.div`
