d2081fb24d1fd213e11b7b2026b18c12c9fb9690
Nat Chen <chen_natalie@outlook.com>
2022-10-08 17:30:10 +0800

    React: move socke event listener to ConversationPage

--- client/src/pages/ConversationPage.tsx
+++ client/src/pages/ConversationPage.tsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useParams } from 'react-router-dom';
 import { ConversationPanel } from '../components/conversations/ConversationPanel';
 import { ConversationSidebar } from '../components/conversations/ConversationSidebar';
 import { AppDispatch, RootState } from '../store';
-import { fetchConversationsThunk } from '../store/conversationSlice';
+import { fetchConversationsThunk, updateConversation } from '../store/conversationSlice';
+import { addMessage } from '../store/messageSlice';
+import { SocketContext } from '../utils/context/SocketContext';
 import { Page } from '../utils/styles';
-import { ConversationType } from '../utils/types';
+import { ConversationType, MessageEventPayload } from '../utils/types';
 
 export const ConversationPage = () => {
   const { id } = useParams();
   const [conversations] = useState<ConversationType[]>([]);
   const dispatch = useDispatch<AppDispatch>();
+
+  const socket = useContext(SocketContext);
+
   const conversationsState = useSelector((state: RootState) => state.conversation.conversations);
 
   useEffect(() => {
@@ -19,6 +24,27 @@ export const ConversationPage = () => {
     console.log(conversationsState.find((c) => c.id === 15));
     dispatch(fetchConversationsThunk());
   }, []);
+
+  useEffect(() => {
+    socket.emit('onClientConnect', {
+      conversationId: parseInt(id!),
+    });
+    socket.on('connected', (data) => {
+      console.log('Connected to Websocket');
+      console.log(data);
+    });
+    socket.on('onMessage', (payload: MessageEventPayload) => {
+      console.log('Message Received');
+      const { conversation, message } = payload;
+      console.log(conversation, message);
+      dispatch(addMessage(payload));
+      dispatch(updateConversation(conversation));
+    });
+    return () => {
+      socket.off('connected');
+      socket.off('onMessage');
+    };
+  }, [id]);
 
   return (
     <Page>
