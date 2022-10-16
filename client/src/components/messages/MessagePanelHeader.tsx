9f665ee1a4d48ec3d5cb2b24fd7f351ff64afc78
Nat Chen <chen_natalie@outlook.com>
2022-10-16 11:24:30 +0800

    React: create GroupRecipientAddForm for adding recipients to group

--- client/src/components/messages/MessagePanelHeader.tsx
+++ client/src/components/messages/MessagePanelHeader.tsx
@@ -1,5 +1,5 @@
 import { PersonAdd } from 'akar-icons';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { RootState } from '../../store';
@@ -8,10 +8,12 @@ import { selectGroupById } from '../../store/groupSlice';
 import { selectType } from '../../store/selectedSlice';
 import { AuthContext } from '../../utils/context/AuthContext'
 import { MessagePanelHeaderStyle } from '../../utils/styles'
+import { AddGroupRecipientModal } from '../modals/AddGroupRecipientModal';
 
 export const MessagePanelHeader = () => {
   const { user } = useContext(AuthContext);
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
   const type = useSelector(selectType);
   const conversation = useSelector(
     (state: RootState) => selectConversationById(state, parseInt(id!))
@@ -26,11 +28,21 @@ export const MessagePanelHeader = () => {
   const groupName = group?.title || 'Group';
   const headerTitle = type === 'group' ? groupName : displayName;
   return (
-    <MessagePanelHeaderStyle>
-      <div>
-        <span>{headerTitle}</span>
-      </div>
-      {type === 'group' && <PersonAdd size={30} />}
-    </MessagePanelHeaderStyle>
+    <>
+      {showModal && (
+        <AddGroupRecipientModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+      )}
+      <MessagePanelHeaderStyle>
+        <div>
+          <span>{headerTitle}</span>
+        </div>
+        {type === 'group' && (
+          <PersonAdd size={30} onClick={() => setShowModal(true)} />
+        )}
+      </MessagePanelHeaderStyle>
+    </>
   );
 }
