138fb195a1339b5c91ca107f4c41fde1cb70013b
Nat Chen <chen_natalie@outlook.com>
2022-10-19 17:00:48 +0800

    Nest: implement endpoint to get friends

--- server/src/friends/friends.controller.ts
+++ server/src/friends/friends.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
-import { Routes } from 'src/utils/constants';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { Routes, Services } from 'src/utils/constants';
+import { AuthUser } from 'src/utils/decorators';
+import { User } from 'src/utils/typeorm';
+import { IFriendsService } from './friends';
 
 @Controller(Routes.FRIENDS)
 export class FriendsController {
-  constructor() {}
+  constructor(
+    @Inject(Services.FRIENDS_SERVICE)
+    private readonly friendsService: IFriendsService,
+  ) {}
+
+  @Get()
+  getFriends(@AuthUser() user: User) {
+    return this.friendsService.getFriends(user.id);
+  }
 }
