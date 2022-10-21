597d8b99da28f80009e44ecbd85a2d7d6f29eb2b
Nat Chen <chen_natalie@outlook.com>
2022-10-21 11:29:57 +0800

    React: add clickable toast when friend request is received

--- client/src/utils/hooks/useToast.tsx
+++ client/src/utils/hooks/useToast.tsx
@@ -1,7 +1,12 @@
 import { toast, ToastOptions } from 'react-toastify';
 
-export function useToast(options?: ToastOptions<{}>) {
-  const success = (data: string) => toast(data, { ...options, type: 'success' });
-  const error = (data: string) => toast(data, { ...options, type: 'error' });
-  return { success, error };
+export function useToast(defaultOptions?: ToastOptions<{}>) {
+  const success = (data: string) => toast(data, { ...defaultOptions, type: 'success' });
+
+  const error = (data: string) => toast(data, { ...defaultOptions, type: 'error' });
+
+  const info = (data: string, options?: ToastOptions<{}>) =>
+    toast(data, { ...defaultOptions, ...options, type: 'error' });
+
+  return { success, error, info };
 }
